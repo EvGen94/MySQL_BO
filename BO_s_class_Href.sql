19207dc234035dd8701c1c6df421360469d2ecc0
Evgenii <51719953+EvGen94@users.noreply.github.com>
2020-01-09 10:36:56 +0300

    orig_sql_rus_com

--- BO_s_class_Href.sql
+++ BO_s_class_Href.sql
@@ -1,21 +1,21 @@
 CREATE PROCEDURE Super_class (in TS varchar(255))
-comment 'БО отчет супер класс' LANGUAGE SQL NOT DETERMINISTIC MODIFIES SQL DATA SQL SECURITY DEFINER
+comment 'Р‘Рћ РѕС‚С‡РµС‚ СЃСѓРїРµСЂ РєР»Р°СЃСЃ' LANGUAGE SQL NOT DETERMINISTIC MODIFIES SQL DATA SQL SECURITY DEFINER
 begin
 
-/*--- строка из БО  call Super_class(CONCAT(@prompt('Коды ТС','A',, multi, free))) ---*/
+/*--- СЃС‚СЂРѕРєР° РёР· Р‘Рћ  call Super_class(CONCAT(@prompt('РљРѕРґС‹ РўРЎ','A',, multi, free))) ---*/
 
-/*--------------- блок для формирования правильной строки ТС и для записи ТС во временную таблицу ---------------*/
+/*--------------- Р±Р»РѕРє РґР»СЏ С„РѕСЂРјРёСЂРѕРІР°РЅРёСЏ РїСЂР°РІРёР»СЊРЅРѕР№ СЃС‚СЂРѕРєРё РўРЎ Рё РґР»СЏ Р·Р°РїРёСЃРё РўРЎ РІРѕ РІСЂРµРјРµРЅРЅСѓСЋ С‚Р°Р±Р»РёС†Сѓ ---------------*/
 declare a int;
 declare  TS_string varchar(255) default ' ';
 declare TS_one varchar(255);
 declare  aloop int;
 declare TS_semicolons varchar(255);
-/*--- временная таблица ---*/
+/*--- РІСЂРµРјРµРЅРЅР°СЏ С‚Р°Р±Р»РёС†Р° ---*/
 CREATE TEMPORARY TABLE TS_tab(
     Number_id INT  default 1 ,
     TS_arr varchar(255) );
 
-set TS_semicolons = replace(TS,'Т',';Т');
+set TS_semicolons = replace(TS,'Рў',';Рў');
 set TS_semicolons = RIGHT( TS_semicolons, character_length(TS_semicolons) - 1  );
 set TS = TS_semicolons;
 SELECT (CHAR_LENGTH(TS) - CHAR_LENGTH(REPLACE(TS,';',''))) div CHAR_LENGTH(';')  into a;
@@ -24,19 +24,19 @@ WHILE aloop > 0 DO
 	set TS_string = concat(TS_string,'\'', REPLACE(SUBSTRING(SUBSTRING_INDEX(TS, ';', aloop),
 		CHAR_LENGTH(SUBSTRING_INDEX(TS, ';', aloop -1)) + 1),';', "") ,'\',');
 	set TS_one = REPLACE(SUBSTRING(SUBSTRING_INDEX(TS, ';', aloop),CHAR_LENGTH(SUBSTRING_INDEX(TS, ';', aloop -1)) + 1),';', "");
-    INSERT INTO TS_tab (Number_id,TS_arr) VALUES (aloop,TS_one); /* -- запись ТС(ов) в TS_arr*/
+    INSERT INTO TS_tab (Number_id,TS_arr) VALUES (aloop,TS_one); /* -- Р·Р°РїРёСЃСЊ РўРЎ(РѕРІ) РІ TS_arr*/
     SET aloop = aloop - 1;    
 END WHILE;
 
 set TS_string = trim(TS_string);
 set TS_string = SUBSTRING(TS_string, 1, character_length(TS_string) - 1 );
 
-/* --------------- для тестов --------------- */
-/* -- select TS_string as 'right_prompt_string' ,a as 'a',aloop as 'aloop'; -- вывод строки ТС(ов) и тестовых данных  */
+/* --------------- РґР»СЏ С‚РµСЃС‚РѕРІ --------------- */
+/* -- select TS_string as 'right_prompt_string' ,a as 'a',aloop as 'aloop'; -- РІС‹РІРѕРґ СЃС‚СЂРѕРєРё РўРЎ(РѕРІ) Рё С‚РµСЃС‚РѕРІС‹С… РґР°РЅРЅС‹С…  */
 /* -- select * from TS_tab -- */
 
-/* --------------- запрос для "БО отчет супер класс" логисты --------------- */
-select item_ref_query.item_num ,  concat('<a href="', concat('mailto:',item_ref_query.email) ,'">',item_ref_query.prod_manager_name2,'</a>') as Логист_под 
+/* --------------- Р·Р°РїСЂРѕСЃ РґР»СЏ "Р‘Рћ РѕС‚С‡РµС‚ СЃСѓРїРµСЂ РєР»Р°СЃСЃ" Р»РѕРіРёСЃС‚С‹ --------------- */
+select item_ref_query.item_num ,  concat('<a href="', concat('mailto:',item_ref_query.email) ,'">',item_ref_query.prod_manager_name2,'</a>') as Р›РѕРіРёСЃС‚_РїРѕРґ 
 from (
     select item_num, ITEM_TS,email, ifNull(zam_logista,prod_manager_name) as prod_manager_name2 
     from item_ref
